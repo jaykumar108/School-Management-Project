4c2647fe8d4546b674b82cc3bba26a245fb6a476
Jay Kumar Sharma <157815432+jaykumar108@users.noreply.github.com>
2025-07-05 07:52:04 +0530

    updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -39,6 +39,7 @@ function App() {
               <Sidebar />
               <SubadminList />
             </>
+            
           } />
           <Route path="/admin-settings" element={
             <>
